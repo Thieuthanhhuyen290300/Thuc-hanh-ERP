dff69943df5dc0d44159ad3c156979e72a910905
Thiều Thanh Huyền <181121521218@due.udn.vn>
2021-11-10 09:42:48 +0700

    sửa đổi

--- ktx/models/student.py
+++ ktx/models/student.py
@@ -2,14 +2,18 @@ from odoo import _, models, fields, api
 
 
 class HrEmployee(models.Model):
-    _inherit = "hr.employee"
+    _inherit = "res.partner"
 
     msv = fields.Char("Mã sinh viên", required=True)
     date_in = fields.Date("Ngày nhập học")
-    sdt=fields.Char("Di động")
-    x_email=fields.Char("Email")
     ten_lien_he=fields.Char("Tên Bố/Mẹ")
-    ngay_cmd=fields.Date("Ngày cấp")
-    name=fields.Char( "Tên sinh viên",placeholder = "Tên sinh viên", required = "True")
+    ngay_cmd=fields.Date("Ngày cấp CMND")
     stk=fields.Char("Sô tài khoản ngân hàng")
-    gvcn=fields.Char("Giáo viên chủ nhiệm")
+    cmnd=fields.Integer("Số CMND")
+    gt = fields.Selection([
+        ('one', 'Nam'),
+        ('two', 'Nữ'),
+        ('three', 'Khác'),], string='Giới tính')
+    sinh=fields.Date("Ngày sinh")
+    dia_chi=fields.Char("Địa Chỉ")
+    sdt_khan=fields.Char("Số điện thoại khẩn")
